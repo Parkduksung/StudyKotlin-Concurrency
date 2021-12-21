9c8331985bc2ffa195c91008b94198b1fe64a36e
Parkduksung <duksung1234@naver.com>
2021-12-22 00:37:36 +0900

    [chapter1] Exam 으로 순차적으로 하기위해 object 로 리팩토링.

--- chapter1/src/main/java/com/rsupport/rv/agent/chapter1/MainActivity.kt
+++ chapter1/src/main/java/com/rsupport/rv/agent/chapter1/MainActivity.kt
@@ -11,30 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        checkTime()
-
     }
 
-    private fun checkTime() = runBlocking {
-        Log.d("결과 - startActiveThread ", Thread.activeCount().toString())
-        val time = measureTimeMillis {
-            createCoroutines(10)
-        }
-        Log.d("결과 - checkTime", time.toString())
-        Log.d("결과 - endActiveThread ", Thread.activeCount().toString())
-    }
-
-    private suspend fun createCoroutines(amount: Int) {
-        val jobs = ArrayList<Job>()
-        for (i in 1..amount) {
-            jobs += CoroutineScope(Dispatchers.IO).launch {
-                Log.d("결과 - startThread ", Thread.currentThread().name)
-                delay(1000L)
-                Log.d("결과 - finishThread ", Thread.currentThread().name)
-            }
-        }
-        jobs.forEach {
-            it.join()
-        }
-    }
 }
