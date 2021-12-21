12b7adfe633ee838c03407defc97997ea1496e55
Parkduksung <duksung1234@naver.com>
2021-12-21 23:52:21 +0900

    [chapter1] 에제1. 코루틴이 스레드 안에 존재하지만 스레드에 얽매이지 않은 가벼운 스레드이다.

--- chapter1/src/main/java/com/rsupport/rv/agent/chapter1/MainActivity.kt
+++ chapter1/src/main/java/com/rsupport/rv/agent/chapter1/MainActivity.kt
@@ -2,15 +2,39 @@ package com.rsupport.rv.agent.chapter1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkTime()
+
+    }
+
+    private fun checkTime() = runBlocking {
+        Log.d("결과 - startActiveThread ", Thread.activeCount().toString())
+        val time = measureTimeMillis {
+            createCoroutines(10)
+        }
+        Log.d("결과 - checkTime", time.toString())
+        Log.d("결과 - endActiveThread ", Thread.activeCount().toString())
     }
 
     private suspend fun createCoroutines(amount: Int) {
-        val jobs = Array<Job>()
-
+        val jobs = ArrayList<Job>()
+        for (i in 1..amount) {
+            jobs += CoroutineScope(Dispatchers.IO).launch {
+                Log.d("결과 - startThread ", Thread.currentThread().name)
+                delay(1000L)
+                Log.d("결과 - finishThread ", Thread.currentThread().name)
+            }
+        }
+        jobs.forEach {
+            it.join()
+        }
     }
 }
