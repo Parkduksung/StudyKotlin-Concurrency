767f5dba9caeda6e5a27c6995e762f98743a200c
Parkduksung <duksung1234@naver.com>
2021-12-30 13:07:13 +0900

    [chapter2][feature] 파싱할때에 networkThread 돌리고 그걸 uiThread 에 합치는 작업.

--- chapter2/src/main/java/com/example/chapter2/MainActivity.kt
+++ chapter2/src/main/java/com/example/chapter2/MainActivity.kt
@@ -1,13 +1,56 @@
 package com.example.chapter2
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val dispatcher = newSingleThreadContext(name = "ServiceCall")
+    private val factory = DocumentBuilderFactory.newInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        GlobalScope.launch(dispatcher) {
+            val resultSize = fetchRssHeadlines().size
+
+            GlobalScope.launch(Dispatchers.Main){
+                Toast.makeText(
+                    this@MainActivity,
+                    resultSize.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            Handler(Looper.getMainLooper()).post {
+                Toast.makeText(
+                    this@MainActivity,
+                    resultSize.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun fetchRssHeadlines(): List<String> {
+        val builder = factory.newDocumentBuilder()
+        val xml = builder.parse("https://www.npr.org/rss/rss.php?id=1001")
+        val news = xml.getElementsByTagName("channel").item(0)
+        return (0 until news.childNodes.length).map { news.childNodes.item(it) }
+            .filter { Node.ELEMENT_NODE == it.nodeType }.map { it as Element }
+            .filter { "item" == it.tagName }
+            .map { it.getElementsByTagName("title").item(0).textContent }
     }
 
 }
