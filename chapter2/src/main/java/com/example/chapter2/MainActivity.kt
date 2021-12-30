aea60566dd1fc0b3e717ae410f2b69a809769944
Parkduksung <duksung1234@naver.com>
2021-12-30 12:02:31 +0900

    [chapter2] init

--- chapter2/src/main/java/com/example/chapter2/MainActivity.kt
+++ chapter2/src/main/java/com/example/chapter2/MainActivity.kt
@@ -1,16 +1,11 @@
-package com.rsupport.rv.agent.chapter1
+package com.example.chapter2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import kotlinx.coroutines.*
-import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Exam3.asyncIncrementTest()
     }
-
 }
--- chapter2/src/test/java/com/example/chapter2/ExampleUnitTest.kt
+++ chapter2/src/test/java/com/example/chapter2/ExampleUnitTest.kt
@@ -1,6 +1,5 @@
-package com.rsupport.rv.agent.chapter1
+package com.example.chapter2
 
-import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -14,15 +13,5 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
-    }
-
-    @Test
-    fun a() = runBlocking {
-
-    }
-
-    @Test
-    fun b() = runBlocking {
-
     }
 }
