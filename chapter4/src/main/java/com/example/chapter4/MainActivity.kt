de39ea161608865db0f81f9085da87f0dfb55652
Parkduksung <duksung1234@naver.com>
2022-01-11 22:33:56 +0900

    [chapter4][feature] Article 형식으로 반환하게 구성.

--- chapter4/src/main/java/com/example/chapter4/MainActivity.kt
+++ chapter4/src/main/java/com/example/chapter4/MainActivity.kt
@@ -2,11 +2,10 @@ package com.example.chapter4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.chapter4.model.Article
 import com.example.chapter4.model.Feed
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.newFixedThreadPoolContext
+import kotlinx.coroutines.*
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import javax.xml.parsers.DocumentBuilderFactory
@@ -22,7 +21,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    private fun asyncFetchHeadlines(feed: Feed, dispatcher: CoroutineDispatcher) =
+    private fun asyncLoadNews() = GlobalScope.launch {
+        val requests = mutableListOf<Deferred<List<Article>>>()
+
+        feeds.mapTo(requests) {
+            asyncFetchArticles(it, dispatcher)
+        }
+
+        requests.forEach {
+            it.join()
+        }
+
+        val articles = requests
+            .filter { !it.isCancelled }
+            .flatMap { it.getCompleted() }
+
+    }
+
+    private fun asyncFetchArticles(feed: Feed, dispatcher: CoroutineDispatcher) =
         GlobalScope.async(dispatcher) {
             val builder = factory.newDocumentBuilder()
             val xml = builder.parse(feed.url)
@@ -30,7 +46,11 @@ class MainActivity : AppCompatActivity() {
             (0 until news.childNodes.length).map { news.childNodes.item(it) }
                 .filter { Node.ELEMENT_NODE == it.nodeType }.map { it as Element }
                 .filter { "item" == it.tagName }
-                .map { it.getElementsByTagName("title").item(0).textContent }
+                .map {
+                    val title = it.getElementsByTagName("title").item(0).textContent
+                    val summary = it.getElementsByTagName("description").item(0).textContent
+                    Article(feed = feed.name, title, summary)
+                }
         }
 
     companion object {
---
+++ chapter4/src/main/java/com/example/chapter4/model/Article.kt
@@ -0,0 +1,7 @@
+package com.example.chapter4.model
+
+data class Article(
+    val feed: String,
+    val title: String,
+    val summary: String
+)
