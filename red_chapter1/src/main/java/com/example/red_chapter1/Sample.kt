eb43972abd30764caeb8dab13e336912e1b6d465
Parkduksung <duksung1234@naver.com>
2022-03-29 18:19:46 +0900

    [feature] chapter1 - CEH와 슈퍼 바이저 잡

--- red_chapter1/src/main/java/com/example/red_chapter1/Sample.kt
+++ red_chapter1/src/main/java/com/example/red_chapter1/Sample.kt
@@ -661,6 +661,105 @@ object Sample {
         }
     }
 
+
+    //어디에도 속하지 않지만 원래부터 존재하는 전역 GlobalScope가 있다.
+    //이 전역 스코프를 이용하면 코루틴을 쉽게 수행할 수 있다.
+    //GlobalScope는 어떤 계층에도 속하지 않고 영원히 동작하게 된다는 문제점이 있다.
+    //프로그래밍에서 전역 객체를 잘 사용하지 않는 것 처럼 GlobalScope도 잘 사용하지 않는다.
+    fun `GlobalScope`() = run {
+        val job = GlobalScope.launch(Dispatchers.IO) {
+            launch { printRandom() }
+        }
+        Thread.sleep(1000L)
+    }
+
+    suspend fun printRandom() {
+        delay(500L)
+        printLog(Random.nextInt(0, 500).toString())
+    }
+
+
+    //scope로 계층적으로 형성된 코루틴을 관리할 수 있다.
+    //필요에 따라 코루틴 스코프를 관리할 수 있다.
+    fun `CoroutineScope`() {
+        val scope = CoroutineScope(Dispatchers.Default)
+        val job = scope.launch(Dispatchers.IO) {
+            launch { printRandom() }
+        }
+        Thread.sleep(1000L)
+    }
+
+
+    //예외를 가장 체계적으로 다루는 방법은 CEH (Coroutine Exception Handler, 코루틴 익셉션 핸들러)를 사용하는 것이다.
+    fun `CEH_코루틴_익셉션_핸들러`() = runBlocking<Unit> {
+        val scope = CoroutineScope(Dispatchers.IO)
+        val job = scope.launch(ceh) {
+            launch { printRandom1() }
+            launch { printRandom2() }
+        }
+        job.join()
+    }
+
+    suspend fun printRandom1() {
+        delay(1000L)
+        printLog(Random.nextInt(0, 500).toString())
+    }
+
+    suspend fun printRandom2() {
+        delay(500L)
+        throw ArithmeticException()
+    }
+
+    // 에러들을 체계적으로 관리할 수 있다.
+    val ceh = CoroutineExceptionHandler { _, exception ->
+        printLog("Something happend: $exception")
+    }
+
+
+    //runBlocking은 자식이 예외로 종료되면 항상 종료되고 CEH를 호출하지 않기에 CEH 를 사용할 수 없다.
+    // 에러 발생할 것임..
+    // java.lang.RuntimeException: Unable to start activity ComponentInfo{com.example.red_chapter1/com.example.red_chapter1.MainActivity}: java.lang.ArithmeticException
+    // Caused by: java.lang.ArithmeticException
+    fun `runBlocking과_CEH`() = runBlocking<Unit> {
+        val job = launch(ceh) {
+            val a = async<String> { printRandom1().toString() }
+            val b = async<String> { printRandom2().toString() }
+            printLog(a.await())
+            printLog(b.await())
+        }
+        job.join()
+    }
+
+
+    //슈퍼 바이저 잡은 예외에 의한 취소를 아래쪽으로 내려가게 한다.
+    //printRandom2가 실패했지만 printRandom1은 제대로 수행된다.
+    //joinAll은 북수개의 Job에 대해 join를 수행하여 완전히 종료될 때까지 기다린다.
+    fun `SupervisorJob`() = runBlocking<Unit> {
+        val scope = CoroutineScope(Dispatchers.IO + kotlinx.coroutines.SupervisorJob() + ceh)
+        val job1 = scope.launch { printRandom1() }
+        val job2 = scope.launch { printRandom2() }
+        joinAll(job1, job2)
+    }
+
+
+    //코루틴 스코프와 슈퍼바이저 잡을 합친듯 한 SupervisorScope
+    fun `SupervisorScope`() = runBlocking<Unit> {
+        val scope = CoroutineScope(Dispatchers.IO)
+        val job = scope.launch {
+            supervisoredFunc()
+        }
+        job.join()
+    }
+
+
+    //슈퍼바이저 스코프를 사용할 때 주의점은 무조건 자식 수준에서 예외를 핸들링 해야한다.
+    //자식의 실패가 부모에게 전달되지 않기 때문에 자식 수준에서 예외를 처리해야합니다.
+    suspend fun supervisoredFunc() = supervisorScope {
+        launch { printRandom1() }
+        launch(ceh) { printRandom2() }
+    }
+
+
     fun printLog(message: String) {
         Log.d(Sample.TAG, message)
     }
