11bface91a009b50a72b28e273e4e950049cdcec
dspark <dspark@rsupport.com>
2022-03-31 17:40:05 +0900

    [feature] chapter1 - 진짜기본적인 활용.

--- red_chapter1/src/main/java/com/example/red_chapter1/MainActivity.kt
+++ red_chapter1/src/main/java/com/example/red_chapter1/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.red_chapter1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import org.w3c.dom.Text
+import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel by lazy {
+        ViewModelProvider(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+                    return MainViewModel() as T
+                } else throw IllegalArgumentException()
+            }
+        }).get(MainViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewModel.mainViewStateLiveData.observe(this) { viewState ->
+            when (viewState) {
+                is MainViewModel.MainViewState.GetData -> {
+                    findViewById<TextView>(R.id.tv_sample).text = viewState.data
+                }
+            }
+        }
     }
 }
