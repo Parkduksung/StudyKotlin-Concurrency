907e17285dfd2102b90048ec23f2b745afe681c2
Parkduksung <duksung1234@naver.com>
2022-04-14 21:55:47 +0900

    [feature] practice1 - titl 변경을 동적으로 하기위해 리스너를 이용했는데.. 별로인듯..

--- practice1/src/main/java/com/example/practice1/ui/MainActivity.kt
+++ practice1/src/main/java/com/example/practice1/ui/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.example.practice1.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.practice1.R
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportFragmentManager.addFragmentOnAttachListener { fragmentManager, _ ->
+            for (fragment in supportFragmentManager.fragments) {
+                if (fragment.isVisible) {
+                    when (fragment) {
+                        is BookSearchFragment -> {
+                            title = "Book Search"
+                        }
+                        is BookDetailFragment -> {
+                            title = "Book Detail"
+                        }
+                    }
+                }
+            }
+        }
+
+        supportFragmentManager.addOnBackStackChangedListener {
+            for (fragment in supportFragmentManager.fragments) {
+                if (fragment.isVisible) {
+                    when (fragment) {
+                        is BookSearchFragment -> {
+                            title = "Book Search"
+                        }
+                        is BookDetailFragment -> {
+                            title = "Book Detail"
+                        }
+                    }
+                }
+            }
+        }
     }
+
+
 }
